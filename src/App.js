38a46429949a086b5f61ed2a5e439c0b5dcf771a
MansiJain06 <mansijain061998@gmail.com>
2021-01-12 22:32:34 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import CreatePage from './pages/CreatePage';
-import Publish from './pages/Publish';
-import pic from './image/pic.png';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import CreatePage from "./pages/CreatePage";
+import Publish from "./pages/Publish";
+import pic from "./image/pic.png";
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <img className="pic" src={pic} alt="image"/>
-      <Switch>
-        <Route path='/' exact component={LandingPage}/>
-        <Route path='/create' exact component={CreatePage}/>
-        <Route path='/publish' exact component={Publish}/>
-    </Switch>
-    </Router>
+      <Router>
+        <img className="pic" src={pic} alt="image" />
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/create" exact component={CreatePage} />
+          <Route path="/publish" exact component={Publish} />
+        </Switch>
+      </Router>
     </div>
   );
 }
